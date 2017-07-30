d335a888a7542f12bf6e1aa4304c6eac554308e9
Rev <romeo.nahon@epitech.eu>
2017-07-30 16:27:13 +0200

    add tileset

---
+++ Ressources/Graphisme/tileSet/TileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TileSet" tilewidth="378" tileheight="378" tilecount="17" columns="17">
+ <image source="TileSet.png" width="6426" height="378"/>
+</tileset>
