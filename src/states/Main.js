fd75601c304966637d2fffc91ce1b5387d0aaab5
sleuvins <sylvain.schellenberger@gmail.com>
2017-07-31 07:51:00 +0200

    Reworked to checkout backwards

--- src/states/Main.js
+++ src/states/Main.js
@@ -1,5 +1,7 @@
 
-var map, mapLayer, ground, walls, light, sprites, player, enemy, torch, path, cursors, actionButton, enemyCallback
+var map, mapLayer, lights, sprites, player, enemy, torch, path, cursors, actionButton, enemyCallback
+var runaway
+
 
 var currentLevel = 1
 const LEVEL_MAX = 5
@@ -34,30 +36,67 @@ class Main extends Phaser.State {
 		//Select the level
 		var levelName = 'level' + currentLevel
 
-		//Creating the map and the layers
+		/*Tiles from 1 to 26: walls
+		*	27 : dark floor
+		* 28 : monster spawn
+		* 29 : player spawn
+		* 30 & 31 : player goals
+		* 32 : it's a trap!
+		* 33 : vent
+		* 34 : blinking light
+		* 35 : stable light
+		*/
+
+		//MAP
     map = this.add.tilemap(levelName)
 
     map.addTilesetImage('mazeTiles', 'tiles')
 
 		mapLayer = map.createLayer('mazeLayer')
 
+		lights = map.createBlankLayer('lights', 50, 50, cellSize, cellSize)
+
+		for (var j = 0; j < mapLayer.layer.height; j++) {
+			for (var i = 0; i < mapLayer.layer.width; i++) {
+
+				switch (mapLayer.layer.data[j][i].index) {
+					case 28:
+						enemy = new Entity(game, cellSize * (i + 0.5), cellSize * (j + 0.5), 'dude', 180, this.enemyCallback)
+						map.putTile(27, i, j, mapLayer)
+						break
+					case 29:
+						player = new Entity(game, cellSize * (i + 0.5), cellSize * (j + 0.5), 'dude', 150, this.playerCallBack)
+						map.putTile(35, i, j, mapLayer)
+						break
+					case 35:
+						map.putTile(27, i, j, lights)
+						break
+					default:
+						break
+				}
+			}
+		}
+
 		mapLayer.scale.set(scale)
+		lights.scale.set(scale)
 
 		mapLayer.resizeWorld()
+
+
 
 		this.physics.startSystem(Phaser.Physics.ARCADE)
 
 		path = new PathFinder(map)
 
-		enemyCallback = this.enemyCallback
-
 		path.displayGrid()
 
 		sprites = this.add.group()
+		sprites.addChild(lights)
 
 		sprites.scale.set(scale);
 
-		enemy = new Entity(game, cellSize * 2.5, cellSize * 1.5, 'dude', 180, this.enemyCallback)
+		//ENEMY
+		//enemy = new Entity(game, cellSize * 2.5, cellSize * 1.5, 'dude', 180, this.enemyCallback)
 		sprites.addChild(enemy)
 		enemy.anchor = {x: 0.5, y: 0.33}
 		this.physics.enable(enemy, Phaser.Physics.ARCADE)
@@ -65,17 +104,23 @@ class Main extends Phaser.State {
 		enemy.animations.add('turn', [4], 20, true)
 		enemy.animations.add('right', [5, 6, 7, 8], 10, true)
 
+		enemyCallback = this.enemyCallback
+		this.runaway = 0
 
-		//Add torch
+		//TORCH
 		torch = this.add.sprite(cellSize * 1.5, cellSize * 1.5, 'torch')
 		torch.animations.add('full', [2])
 		torch.animations.add('off', [0])
 		torch.animations.add('low', [1])
 		sprites.addChild(torch)
-		torch.bringToTop()
+		//torch.moveUp()
 		torch.anchor = {x: 0.5, y: 0.5}
 
-    player = new Entity(game, cellSize * 1.5, cellSize * 1.5, 'dude')
+		//Lighted tiles
+		lights.bringToTop()
+
+		//PLAYER
+    //player = new Entity(game, cellSize * 1.5, cellSize * 1.5, 'dude', 150, this.playerCallBack)
 		player.anchor = {x: 0.5, y: 0.33}
 		sprites.addChild(player)
 		player.bringToTop()
@@ -142,9 +187,11 @@ class Main extends Phaser.State {
 			var scopeThreshold2 = cellSize*cellSize*4
 			var angleThreshold = Math.PI * 0.1
 
+			//If monster is in the light beam:
 			if(monsterDistance2 < scopeThreshold2 && Math.abs(monsterAngle - viewAngle) < angleThreshold)
 			{
-				console.log("Aie, ça fait mal!!!!");
+				//Monster is confused and run away (10 steps):
+				this.runaway = 10;
 			}
 
 		}
@@ -157,26 +204,106 @@ class Main extends Phaser.State {
 		game.debug.body(player)
 	}*/
 
-	enteredCorridor() {
-		console.log("It's a corridor")
+	//Function called when the enemy use a tp:
+	enemyEntersTp(x,y){
+
+		//TODO: hide the monster!
+
+		console.log("Enemy enters in the TP!");
+
+		//Move the monster to the given coordinates:
+		enemy.targetX = x
+		enemy.targetY = y
+
+		enemy.body.x = x * cellSize
+		enemy.body.y = y * cellSize
+
+		//Plan the exit of the Tp (a few seconds later):
+		setTimeout(enemyQuitsTp, 2000 + Math.random()*2000);
+	}
+
+
+	//Function called when the enemy get out of a tp:
+	enemyQuitsTp(){
+
+		//TODO: display the monster!
+
+		console.log("Enemy quits the TP!");
+
+		//Resume the monster's behavior:
+		enemyCallback();
 	}
 
 	enemyCallback () {
 
+		//Enemy track the player. With high probability, the monster is able
+		//to find the precise location of the player:
 		if(Math.random() < 0.7)
 			path.computeDistances(player.targetX, player.targetY)
-		var dir = path.bestDir(enemy.targetX, enemy.targetY)
 
-		if(dir < 4)
+		var dir = 4 //default dir is 4 (4 means 'NONE').
+
+		//If the monster is running away:
+		if(this.runaway > 0)
 		{
-			console.log('enemy move ',dirNum[dir]);
-			enemy.move(dirNum[dir])
+			//Monster tries to avoid player:
+			dir = path.worstDir(enemy.targetX, enemy.targetY)
+			this.runaway--
+			console.log("Monster is Running Away! RunAwayCounter=",this.runaway);
 		}
 		else
 		{
-			 setTimeout(enemyCallback, 250 + Math.random()*500);
+			//Monster try to catch player:
+			dir = path.bestDir(enemy.targetX, enemy.targetY)
+			console.log("Monster is Tracking the Player! RunAwayCounter=",this.runaway);
+		}
+
+		if(dir < 4)
+		{
+
+			if(path.useTp)
+			{
+				console.log("Monster use TP!");
+				//The monster tries to use a TP so don't move immediately:
+				var x = path.coordX(path.tpPos)
+				var y = path.coordY(path.tpPos)
+				enemyEntersTp(x,y)
+			}
+			else
+			{
+				console.log("Monster Regular move!");
+				//Regular move:
+				console.log('enemy move ',dirNum[dir]);
+				enemy.move(dirNum[dir])
+			}
+		}
+		else
+		{
+			console.log("Monster has lost the player! Waiting...");
+			//No moves done, wait a little, then try again!
+			setTimeout(enemyCallback, 250 + Math.random()*500);
 		}
 	}
+
+	playerCallback() {
+		if (player.tileType == 30) {
+			if (currentLevel < LEVEL_MAX) {
+				currentLevel++
+			}
+			this.state.start('Main')
+		}
+	}
+
+	/*Tiles from 1 to 26: walls
+	*	27 : dark floor
+	* 28 : monster spawn
+	* 29 : player spawn
+	* 30 & 31 : player goals
+	* 32 : it's a trap!
+	* 33 : vent
+	* 34 : blinking light
+	* 35 : stable light
+	*/
 
 }
 
